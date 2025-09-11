e13553dca90da331e24ed9fb49fdc1e887b75de0
Atul Shahwal <atulshahwal880@gmail.com>
2025-09-11 12:48:39 +0530

    c

--- src/main/java/org/desingpatterns/questions/cabbookingsystem/interview/OnlineCabBookingSystemInterview.java
+++ src/main/java/org/desingpatterns/questions/cabbookingsystem/interview/OnlineCabBookingSystemInterview.java
@@ -608,7 +608,28 @@ class DistanceBasedMatching implements MatchingStrategy {
 
         return closestDriver;
     }
-
+    /**
+     * Interview Q&A: How does the Haversine formula work for distance calculation?
+     * Answer:
+     * The Haversine formula calculates the great-circle distance between two points
+     * on a sphere given their latitudes and longitudes. It is more accurate than using
+     * simple Euclidean distance because it accounts for the Earth's curvature.
+     *
+     * Mathematical formula:
+     *
+     * a = sin²(Δφ / 2) + cos(φ1) * cos(φ2) * sin²(Δλ / 2)
+     * c = 2 * atan2(√a, √(1 − a))
+     * d = R * c
+     *
+     * where:
+     * - φ1, φ2 are the latitudes of the two points in radians
+     * - λ1, λ2 are the longitudes of the two points in radians
+     * - Δφ = φ2 - φ1 is the difference in latitude
+     * - Δλ = λ2 - λ1 is the difference in longitude
+     * - R is the Earth's radius (mean radius = 6371 km)
+     * - d is the calculated distance between the two points along the surface of the sphere
+     *
+     */
     private double calculateDistance(Location loc1, Location loc2) {
         final int R = 6371; // Earth's radius in km
 
